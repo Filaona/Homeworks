d4ef7855632dc81e286ec2964d2899f7d1124fc5
Filaona <di.superanskaya@yandex.ru>
2017-01-31 10:09:52 +0300

    Add files via upload

---
+++ python_variant4/hw_9/hw_9.py
@@ -0,0 +1,25 @@
+import re
+
+def split_text(file_name):
+    file = open(file_name, 'r', encoding = 'utf-8')
+    list_words = []
+    string = file.read()
+    string.lower()
+    if string:
+            for word in string.split():
+                word = word.strip()
+                list_words.append(word + ' '.strip('.,?!()'))
+    file.close()
+    return list_words
+
+def check(list_words):
+    for word in list_words:
+        if re.match(r'вып((ит((а|о|ы|ого|ому|ая|ую|ою|ых|ыми|((о|ы)(й|м|е)))?))|(ивш(и|их|ими|ею|его|ему|ая|ую|((и|е)(й|м|е))))|(ил((и|а|о)?))|(ь((ю(т?))|(е(шь|т|м|те))))|(ей(те)?)|ив) ', word):
+            print(word)
+
+
+def main():
+    list_words = split_text(input('Введите название файла: '))
+    check(list_words)
+
+main()
